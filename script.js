aa0804a5f396f834843ec5a687a096f7eb0727c2
“mugishaj092” <mugishajoseph092@gmail.com>
2024-02-28 14:40:42 +0200

    eventlistener for operators

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const display = document.querySelector('input[name="display"]');
+  const display = document.querySelector('input[name="display"]');
 
-    function appendToDisplay(value) {
-        display.value += value;
-    }
+  function appendToDisplay(value) {
+    display.value += value;
+  }
 
-    function clearDisplay() {
-        display.value = '';
-    }
+  function clearDisplay() {
+    display.value = "";
+  }
 
-    function addOperator(operator) {
-        appendToDisplay(operator);
-    }
+  function addOperator(operator) {
+    appendToDisplay(operator);
+  }
 
-    function calculateResult() {
-        display.value = eval(display.value);
-    }
-
+  function calculateResult() {
+    display.value = eval(display.value);
+  }
+  document
+    .getElementById("operator-ac")
+    .addEventListener("click", clearDisplay);
+  document
+    .getElementById("operator-del")
+    .addEventListener("click", function () {
+      display.value = display.value.slice(0, -1);
+    });
+  document
+    .getElementById("operator-dot")
+    .addEventListener("click", function () {
+      appendToDisplay(".");
+    });
+  document
+    .getElementById("operator-divide")
+    .addEventListener("click", function () {
+      addOperator("/");
+    });
+  document
+    .getElementById("operator-multiply")
+    .addEventListener("click", function () {
+      addOperator("*");
+    });
+  document
+    .getElementById("operator-subtract")
+    .addEventListener("click", function () {
+      addOperator("-");
+    });
+  document
+    .getElementById("operator-add")
+    .addEventListener("click", function () {
+      addOperator("+");
+    });
+  document
+    .getElementById("operator-equal")
+    .addEventListener("click", calculateResult);
 });
