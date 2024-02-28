40887c827f8eb76c717ab5452c5c8d0b330da045
“mugishaj092” <mugishajoseph092@gmail.com>
2024-02-28 14:46:37 +0200

    fixing opertors errors

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const display = document.querySelector('input[name="display"]');
+    const display = document.querySelector('input[name="display"]');
 
-  function appendToDisplay(value) {
-    display.value += value;
-  }
+    function appendToDisplay(value) {
+        display.value += value;
+    }
 
-  function clearDisplay() {
-    display.value = "";
-  }
+    function clearDisplay() {
+        display.value = '';
+    }
 
-  function addOperator(operator) {
-    appendToDisplay(operator);
-  }
+    function add() {
+        appendToDisplay('+');
+    }
 
-  function calculateResult() {
-    display.value = eval(display.value);
-  }
-  const numberButtons = [
-    "00",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
+    function subtract() {
+        appendToDisplay('-');
+    }
 
-  numberButtons.forEach((number) => {
-    document
-      .getElementById(`number-${number}`)
-      .addEventListener("click", function () {
-        appendToDisplay(number);
-      });
-  });
+    function multiply() {
+        appendToDisplay('*');
+    }
+
+    function divide() {
+        appendToDisplay('/');
+    }
+
+    function calculateResult() {
+        display.value = eval(display.value);
+    }
+
+    document.getElementById('operator-ac').addEventListener('click', clearDisplay);
+    document.getElementById('operator-del').addEventListener('click', function () {
+        display.value = display.value.slice(0, -1);
+    });
+    document.getElementById('operator-dot').addEventListener('click', function () {
+        appendToDisplay('.');
+    });
+    document.getElementById('operator-divide').addEventListener('click', divide);
+    document.getElementById('operator-multiply').addEventListener('click', multiply);
+    document.getElementById('operator-subtract').addEventListener('click', subtract);
+    document.getElementById('operator-add').addEventListener('click', add);
+    document.getElementById('operator-equal').addEventListener('click', calculateResult);
+
+    const numberButtons = ['00', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    numberButtons.forEach((number) => {
+        document
+            .getElementById(`number-${number}`)
+            .addEventListener('click', function () {
+                appendToDisplay(number);
+            });
+    });
 });
